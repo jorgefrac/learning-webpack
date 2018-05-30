b8e65d6452257a45f32973de8d729a729873f2e7
Jorge Fco <jorge.fco.mend@gmail.com>
2018-05-29 23:37:14 -0500

    Update webpack CLI

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import _ from 'lodash';
-import 		'./css/icons.css';
-import 		'./css/framework.css';
+//import _ from 	'lodash';
+//import 			'./css/icons.css';
+//import 			'./css/framework.css';
+//import css from './css/framework.css';
 
+require('./css/framework.css');
 
